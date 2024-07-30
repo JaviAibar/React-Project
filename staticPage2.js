f476672982a66ea3ab6424d94c38659cece0c7c8
JaviAibar <es.javi.es@gmail.com>
2024-07-30 11:30:27 +0200

    Second example

---
+++ staticPage2.js
@@ -0,0 +1,12 @@
+const myNavBar = (
+    <nav>
+        <h1>JaviSites</h1>
+        <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(myNavBar, document.getElementById("root"))
