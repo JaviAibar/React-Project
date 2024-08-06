926d502f73f112853ea3677eef7447e4fecf388b
JaviAibar <es.javi.es@gmail.com>
2024-08-06 21:17:07 +0200

    S8

--- index.jsx
+++ index.jsx
@@ -4,3 +4,5 @@ import { createRoot } from 'react-dom/client';
 // Render your React component instead
 /*const root = createRoot(document.getElementById('root'));
 root.render(<h1>Hello, world</h1>);*/
+
+
