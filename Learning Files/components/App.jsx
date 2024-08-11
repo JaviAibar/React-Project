1707b9e828322141db440d10d3d8de51ebb791ca
JaviAibar <es.javi.es@gmail.com>
2024-08-11 18:28:18 +0200

    Copyied Vite default project to adquire eslint and other complements in order to make coding more efficient (not importing React in every file)

---
+++ Learning Files/components/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import React from "react"
+import OtherComponent from "./OtherComponent"
+import Footer from "./Footer"
+export default function App() {
+ return (
+    <>
+    <Header />
+    <OtherComponent />
+    <Footer />
+    </>
+    
+ )
+}
